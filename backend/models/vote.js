1dc190b728cd57e07549940d1535b2015914fdcb
Sujan Rao Chikkela <sujan@Sujans-MacBook-Pro.local>
2022-04-30 10:22:14 -0700

    Modified Votes Schema

--- backend/models/vote.js
+++ backend/models/vote.js
@@ -5,7 +5,7 @@ const voteSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, required: true },
     vote: { type: Number, required: true },
-    created: { type: Date, default: Date.now }
+    created: { type: Date, default: Date.now } 
   },
   { _id: false }
 );
