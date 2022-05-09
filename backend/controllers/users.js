38ab079a0730167fc665841b725c2a9ca8a40707
Rakshith Reddy Bollu <rakshithreddy.bollu@sjsu.edu>
2022-05-09 13:10:18 -0700

    fixed the userstats and activity API

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,14 +1,13 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-const { body, validationResult } = require('express-validator');
-const jwt = require('jsonwebtoken');
-const {secret} = require('../utils/config');
-const bcrypt = require('bcryptjs');
-const {auth} = require('../utils/passport');
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const { secret } = require("../utils/config");
+const bcrypt = require("bcryptjs");
+const { auth } = require("../utils/passport");
 auth();
 
 const signup = async (req, res) => {
-
   const result = validationResult(req);
   if (!result.isEmpty()) {
     const errors = result.array({ onlyFirstError: true });
@@ -16,60 +15,65 @@ const signup = async (req, res) => {
   }
 
   try {
-    const { email,password } = req.body;
+    const { username, email, password } = req.body;
 
     const existingUsername = await User.findOne({
-      email: email
+      email: email,
     });
 
-    console.log('sunnyk' + existingUsername);
+    console.log("sunnyk" + existingUsername);
 
     if (existingUsername) {
       res.status(400).json({
-        message: 'Username already exists.'
+        message: "Username already exists.",
       });
-      console.log('Username already exists.');
-    }
-    else
-    {
-
-    const newUser = new User({email,password});
-    const salt = await bcrypt.genSalt(10);
-    newUser.password = await bcrypt.hash(password, salt);
-    const savedUser = await newUser.save();
-
-    if (savedUser) {
-
-      const { email, role, id, created, profilePhoto,location,lastseen } = savedUser;
-      const userInfo = {
-        email,
-        role,
-        id,
-        created,
-        profilePhoto,
-        location,
-        lastseen
-      };
-
-     res.json({
-        message: 'User created!',
-        userInfo
-      });
+      console.log("Username already exists.");
     } else {
-      res.status(400).json({
-        message: 'There was a problem creating your account.'
-      });
+      const newUser = new User({ username, email, password });
+      const salt = await bcrypt.genSalt(10);
+      newUser.password = await bcrypt.hash(password, salt);
+      const savedUser = await newUser.save();
+
+      if (savedUser) {
+        const {
+          username,
+          email,
+          role,
+          id,
+          created,
+          profilePhoto,
+          location,
+          lastseen,
+        } = savedUser;
+        const userInfo = {
+          email,
+          role,
+          id,
+          created,
+          profilePhoto,
+          location,
+          lastseen,
+          username,
+        };
+
+        res.json({
+          message: "User created!",
+          userInfo,
+        });
+      } else {
+        res.status(400).json({
+          message: "There was a problem creating your account.",
+        });
+      }
     }
-  }} catch (error) {
+  } catch (error) {
     res.status(400).json({
-      message: 'There was a problem creating your account.'
+      message: "There was a problem creating your account.",
     });
   }
 };
 
-
 const login = async (req, res) => {
-
   const result = validationResult(req);
   if (!result.isEmpty()) {
     const errors = result.array({ onlyFirstError: true });
@@ -77,42 +81,49 @@ const login = async (req, res) => {
   }
   try {
     const { email, password } = req.body;
-    await User.findOneAndUpdate({email:email},{email:email,lastseen:Date.now()},{upsert:true, new:true});
-    const user = await User.findOne({
-      email: email
-    }, {}, {lean: true});
+    await User.findOneAndUpdate(
+      { email: email },
+      { email: email, lastseen: Date.now() },
+      { upsert: true, new: true }
+    );
+    const user = await User.findOne(
+      {
+        email: email,
+      },
+      {},
+      { lean: true }
+    );
     if (!user) {
       res.status(403).json({
-        message: 'Wrong username or password.'
+        message: "Wrong username or password.",
       });
     }
 
     //const passwordValid = await verifyPassword(password, user.password);
-    bcrypt.compare(password, user.password, function(err, isMatch){
+    bcrypt.compare(password, user.password, function (err, isMatch) {
       if (err) {
-          throw err
-        } else if (!isMatch) {
-          res.status(403).json({
-            message: 'password did not match'
-          });
-        } else {
-          const payload = {id : user.id,email : user.email};
-          const token = jwt.sign(payload,secret, {
-           expiresIn: 10080000
-          });
-          res.cookie('so_token', token, { httpOnly: true });
-          res.status(200).json({token: "JWT " + token, user, success: true});
-        }
-      });
-    }  
- catch (error) {
+        throw err;
+      } else if (!isMatch) {
+        res.status(403).json({
+          message: "password did not match",
+        });
+      } else {
+        const payload = { id: user.id, email: user.email };
+        const token = jwt.sign(payload, secret, {
+          expiresIn: 10080000,
+        });
+        res.cookie("so_token", token, { httpOnly: true });
+        res.status(200).json({ token: "JWT " + token, user, success: true });
+      }
+    });
+  } catch (error) {
     return res.status(500).json({
-      message: 'Something went wrong.'
+      message: "Something went wrong.",
     });
   }
 };
 
 module.exports = {
-	signup,
-	login
+  signup,
+  login,
 };
